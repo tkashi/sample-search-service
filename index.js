e543e0cfeffa832dc7f818d16033bdac5c1efc46
tkashi <takuksmr@amazon.co.jp>
2022-03-03 08:19:03 +0900

    add presigned url for s3

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const express = require('express')
-const app = express()
-const { KendraClient, QueryCommand } = require('@aws-sdk/client-kendra')
 const { query } = require('express')
+
+const { KendraClient, QueryCommand } = require('@aws-sdk/client-kendra')
+const { HttpRequest } = require("@aws-sdk/protocol-http")
+const { S3RequestPresigner } = require("@aws-sdk/s3-request-presigner")
+const { parseUrl } = require("@aws-sdk/url-parser")
+const { Hash } = require("@aws-sdk/hash-node")
+const { formatUrl } = require("@aws-sdk/util-format-url");
+
+const getPresignedUrl = async (s3Url) => {
+  console.log(s3Url);
+  const presigner = new S3RequestPresigner({
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
+      region: 'us-east-1',
+      sha256: Hash.bind(null, "sha256")
+  });
+  // Create a GET request from S3 url.
+  const signedReq = await presigner.presign(new HttpRequest(parseUrl(s3Url)));
+  console.log(formatUrl(signedReq));
+  return formatUrl(signedReq);
+}
+
+const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
@@ -13,7 +36,7 @@ app.get('/api/search', async (req, res, next) => {
     const client = new KendraClient({ region: 'us-east-1'})
     const params = {
         QueryText: req.query.keyword,
-        IndexId: process.env.KENDRA_INDEX
+        IndexId: process.env.KENDRA_INDEX 
     };
 
     if (req.query.category) {
@@ -31,7 +54,12 @@ app.get('/api/search', async (req, res, next) => {
 
     try {
         const data = await client.send(command);
-        res.json(data.ResultItems)
+        const resultItems = data.ResultItems;
+        for await (item of resultItems) {
+          const s3Url = item.DocumentURI;
+          item.DocumentURI = await getPresignedUrl(s3Url);
+        }
+        res.json(resultItems)
       } catch (error) {
         next (error)
       }
